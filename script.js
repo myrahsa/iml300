9a71096be113b2b1a66495e8a3f4e466d1b91a81
myrahsa <78187389+myrahsa@users.noreply.github.com>
2021-05-06 06:29:18 -0700

    redesign

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	
+	
+    $("#hover-3a").hover(function () {
+        $("#preview-3a").toggle();
+    })
+	
+	    $("#hover-hn").hover(function () {
+        $("#preview-hn").toggle();
+    })
+	
+	
+	
+})
